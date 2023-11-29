0186f929089dcd36cbb3d431d79e4c4312ae8332
d.pilatsis@ll-games.com <d.pilatsis@ll-games.com>
2023-11-30 02:26:57 +0400

    step 6

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 dev-run:
-	docker-compose -f docker-compose.override.yml up app  
+	docker-compose -f docker-compose.override.yml up   
 
 ci: 
-	docker-compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app app
+	docker-compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app
 
 image-build: 
 	docker-compose -f docker-compose.yml build app
--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,10 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-  },
   production: {
     dialect: 'postgres',
     database: process.env.DATABASE_NAME,
@@ -12,23 +8,14 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
+
   test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+     dialect: 'postgres',
+     database: process.env.DATABASE_NAME,
+     username: process.env.DATABASE_USERNAME,
+     password: process.env.DATABASE_PASSWORD,
+     port: process.env.DATABASE_PORT,
+     host: process.env.DATABASE_HOST,
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
