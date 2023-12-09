c8b545416dc163a119b74eac2be5e58b4627c421
d.pilatsis@ll-games.com <d.pilatsis@ll-games.com>
2023-12-09 20:35:04 +0400

    sd

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,14 +1,19 @@
 require('dotenv').config();
 
 module.exports = {
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-  },
+//   production: {
+//     dialect: 'postgres',
+//     database: process.env.DATABASE_NAME,
+//     username: process.env.DATABASE_USERNAME,
+//     password: process.env.DATABASE_PASSWORD,
+//     port: process.env.DATABASE_PORT,
+//     host: process.env.DATABASE_HOST,
+//   },
+
+   production: {
+      dialect: 'sqlite',
+      storage: './database.test.sqlite',
+    },
 
   test: {
     dialect: 'sqlite',
